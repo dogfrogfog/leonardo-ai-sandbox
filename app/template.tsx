10e452fe2992fb52d4c66e42f58e7f0871970915
maksim hodasevich <maksim.hodasevich@gmail.com>
2023-08-25 17:20:20 +0200

    add react query

--- app/template.tsx
+++ app/template.tsx
@@ -1,7 +1,15 @@
 "use client";
 import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { GenerationProvider } from "./GenerationContext";
 
+const queryClient = new QueryClient();
+
 export default function Template({ children }: { children: ReactNode }) {
-  return <GenerationProvider>{children}</GenerationProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GenerationProvider>{children}</GenerationProvider>
+    </QueryClientProvider>
+  );
 }
